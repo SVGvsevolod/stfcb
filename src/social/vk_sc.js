bd84e2f603a71f20d6ce6be0da919a43e8e396a0
Vsevolod Subbotkin <vsevolod.98t@mail.ru>
2024-03-15 15:10:54 +0200

    wip

--- src/social/vk_sc.js
+++ src/social/vk_sc.js
@@ -1,11 +1,11 @@
 export async function vk_sc() {
     console.log(vk_sc.cache)
-    console.log(await req({
+    vk_sc.cache = JSON.parse(await req({
         body: `access_token=${process.env.vk_k}&user_id=${process.env.vk_u}&v=${process.env.vk_v}`,
         method: 'POST',
         mime: 'application/x-www-form-urlencoded',
         url: 'https://api.vk.com/method/users.getFollowers'
-    }))
+    })).response.count
     console.log(vk_sc.cache)
 }
 vk_sc.cache = 0
