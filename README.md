c2c647f2877db6ab2c46f8a61a926ec382e66114
Vsevolod Subbotkin <vsevolod.98t@mail.ru>
2024-03-20 16:21:03 +0200

    wip

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# stfcb
+[ [README на русском](https://github.com/SVGvsevolod/stfcb/blob/main/README_ru.md) ]
+
+# Stats-to-Flex™ Collect Bot
+Collects periodically followers/subscribers numbers from social networks and stores in PostgreSQL database every hour.
--- src/loop.js
+++ src/loop.js
@@ -2,34 +2,38 @@
 import { IGSC } from './social/IGSC.js'
 import { VKSC } from './social/VKSC.js'
 import { YTSC } from './social/YTSC.js'
-// Counters
+/**
+ * Counters
+ */
 const counts = {}
 /**
  * Main loop
  */
 export function loop() {
+    // Defining counters
     if (!(counts.ig_sc instanceof IGSC))
         counts.ig_sc = new IGSC({
             ids: JSON.parse(process.env.ig_u),
             key: process.env.ig_k,
             tables: JSON.parse(process.env.ig_t)
         })
-    //else
-        //counts.ig_sc.get()
     if (!(counts.vk_sc instanceof VKSC))
         counts.vk_sc = new VKSC({
             ids: JSON.parse(process.env.vk_u),
             key: process.env.vk_k,
             tables: JSON.parse(process.env.vk_t)
         })
-    //else
-        //counts.vk_sc.get()
     if (!(counts.yt_sc instanceof YTSC))
         counts.yt_sc = new YTSC({
             ids: JSON.parse(process.env.yt_u),
             key: process.env.yt_k,
             tables: JSON.parse(process.env.yt_t)
         })
-    //else
+    // Retreiving numbers
+    //if(counts.ig_sc instanceof IGSC)
+        //counts.ig_sc.get()
+    //if(counts.vk_sc instanceof VKSC)
+        //counts.vk_sc.get()
+    //if(counts.yt_sc instanceof YTSC)
         //counts.yt_sc.get()
 }
